3b11022d19d680876a8578cd52a601f76e1fdbf4
Crew Spence <csspence123@gmail.com>
2020-03-06 06:34:28 -0600

    Wrote working solution for summation.

--- summation.js
+++ summation.js
@@ -10,6 +10,11 @@ summation(8) -> 36
 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 */
 
-var summation = function (num) {
-  // Code here
+const summation = (num) => {
+  let sum = 0;
+  for(let i = 1; i < num + 1; i++) {
+    sum += i;
+  }
+
+  return sum;
 }
